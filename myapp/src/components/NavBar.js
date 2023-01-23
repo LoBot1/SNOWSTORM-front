eaf6892b510dc9d46973d9bc1f684c8e3354c852
LoBot1 <logan.bnt4@gmail.com>
2023-01-23 14:23:31 +0100

    feature start

---
+++ myapp/src/components/NavBar.js
@@ -0,0 +1,57 @@
+import { useRef, useState } from "react";
+import {FaBars, FaTimes } from "react-icons/fa";
+import { Link } from "react-router-dom";
+import '../style/main.css'
+// npm install react-icons
+function  NavBar(){
+    const navRef = useRef();
+    const showNavbar = () => {
+        navRef.current.classList.toggle("responsive_nav");
+        
+    }
+    
+    return( 
+        <header>
+            <div>
+                <nav ref={navRef}>
+                    <button className="nav-btn nav-close-btn" onClick={showNavbar}>
+                        <FaTimes/>
+                    </button>
+                    <ul>
+                        <li>
+                            <a href="/Entraineur">Entraineur</a>
+                        </li>
+                    </ul>
+                    <ul>
+                        <li>
+                            <a href="/Camps">Camps</a>
+                        </li>
+                    </ul>
+                    <ul>
+                        <li>
+                            <a href="/home"><img className="logo" src="https://upload.wikimedia.org/wikipedia/fr/thumb/a/a0/Logo_Orl%C3%A9ans_Loiret_Basket_-_2014.svg/1200px-Logo_Orl%C3%A9ans_Loiret_Basket_-_2014.svg.png" alt="elite basket camps"/></a>
+                        </li>
+                    </ul>
+                    <ul>
+                        <li>
+                            <a href="/Shop">Boutique</a>
+                        </li>
+                    </ul>
+                    <ul>
+                        <li>
+                            <a href="/Contact">Contact</a>
+                        </li>
+                    </ul>
+                </nav>
+                <button className="nav-btn" onClick={showNavbar}>
+                    <FaBars/>
+                </button>
+            </div>
+        </header>
+
+
+     );
+}
+
+
+export default NavBar ; 
---
+++ myapp/src/pages/home.js
@@ -0,0 +1,50 @@
+
+import NavBar from '../components/NavBar.js';
+
+import '../style/home.css'
+
+function home() {
+    return (
+        <div className='Contain'>
+            <div className='box1'>
+                <div className='sloganhome'>
+                    <div className='slogan1'>
+                        <div className='slogan'><h2>Elite Basket Camps</h2></div>
+                        <div className='space'></div>
+                    </div>
+                    <div className='slogan2'>
+                        <div className='space'></div>
+                        <div className='slogan'><h2>C'est bien plus que du sport</h2></div>
+                    </div>
+                </div>
+            </div>
+            <div className='box2'>
+                <h2>Nos Entraineur</h2>
+                <img className="test" src="https://thumbs.dreamstime.com/b/jour-de-terre-d-environnement-dans-les-mains-des-arbres-cultivant-jeunes-plantes-bokeh-verdissent-la-main-femelle-fond-tenant-l-130247647.jpg"></img>
+            </div>
+            
+            <div className='box2'>
+                <h2>Nos Camps</h2>
+            </div>
+            
+            <div className='box2'>
+                <h2>Boutique</h2>
+            </div>
+            <div className='button'>
+                <div className='elite'>
+                    <button>
+                    <div><h5>ELITE</h5></div>
+                        <span></span><span></span><span></span><span></span><span></span>
+                    </button>
+                </div>
+                <div className='ludique'>
+                    <button>
+                        <div><h5>LUDIQUE</h5></div>
+                        <span></span><span></span><span></span><span></span><span></span>
+                    </button>
+                </div>
+            </div>
+        </div>
+    )
+}
+export default home;
